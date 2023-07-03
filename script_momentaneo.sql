24ca4394a701197d70f146daec5d9bf56c494cc6
GOLAUS <agolcher10647@ufide.ac.cr>
2023-07-03 17:29:48 -0600

    Update script_momentaneo.sql

--- script_momentaneo.sql
+++ script_momentaneo.sql
@@ -12,3 +12,21 @@ insert into Usuario(usuario,contrasena)
 values('admin','pass');
 
 select * from Usuario;
+ 
+/*paquetes_Viaje*/
+CREATE TABLE paquetes_Viaje (
+    ID INT IDENTITY (1,1) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    destino VARCHAR(100) NOT NULL,
+    duracion_dias INT NOT NULL,
+    precio DECIMAL(10, 2)NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE NOT NULL,
+    descripcion VARCHAR(500) NOT NULL,
+    cantidad_personas INT NOT NULL
+);
+
+INSERT INTO paquetes_Viaje (nombre, destino, duracion_dias, precio, fecha_inicio, fecha_fin, descripcion, cantidad_personas)
+VALUES ('Paquete A', 'Destino A', 7, 1500.00, CONVERT(DATE, '01-08-2023', 105), CONVERT(DATE, '07-08-2023', 105), 'Descripción del paquete A', 2);
+
+select * from paquetes_Viaje;
