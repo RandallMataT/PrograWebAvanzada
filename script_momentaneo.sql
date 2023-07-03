f86086f05ba77cb41e64a3b651f40a7571fa24f6
josepablo27 <jduran60412@ufide.ac.cr>
2023-07-02 22:58:49 -0600

    script

--- script_momentaneo.sql
+++ script_momentaneo.sql
@@ -2,8 +2,9 @@ create database FidelitasComunica
 
 use FidelitasComunica
 
-drop table Usuarios
+drop table Usuario
 create table Usuario(
+ID int primary key IDENTITY(1,1) not null,
 usuario varchar(10) not null,
 contrasena varchar(10) not null);
 
