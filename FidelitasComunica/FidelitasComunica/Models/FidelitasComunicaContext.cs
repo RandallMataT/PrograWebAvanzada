d6f409c327c2da96d31cdc4abcff9c42009d53b5
josepablo27 <jduran60412@ufide.ac.cr>
2023-07-02 18:27:21 -0600

    poryecto creado

---
+++ FidelitasComunica/FidelitasComunica/Models/FidelitasComunicaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FidelitasComunica.Models
+{
+    public class FidelitasComunicaContext : DbContext
+    {
+        public FidelitasComunicaContext(DbContextOptions<FidelitasComunicaContext> opciones) 
+            :base(opciones)
+        {
+        }
+
+        public DbSet<Usuario> Uusario { get; set; }
+    }
+}
---
+++ FidelitasComunica/FidelitasComunica/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace FidelitasComunica.Models
+{
+    public class Usuario
+    {
+
+        public string usuario { get; set; }
+        public string contrasena { get; set; }
+    }
+}
